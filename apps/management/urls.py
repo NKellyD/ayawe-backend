e08976b9eb14c2fdeaff53da5430fcee0b36aedd
kd <niyuhirekderlin@gmail.com>
2026-04-27 16:18:21 +0200

    urls categories

--- apps/management/urls.py
+++ apps/management/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import AccountView
+from .views import AccountView,CategoryTypeView,CategoryView
 
 urlpatterns = [
     path('account/', AccountView.as_view(), name='account'),
+    path('categories-type/', CategoryTypeView.as_view(), name='categories-type'),
+    path('categories/', CategoryView.as_view(), name='categories'),
 ]
