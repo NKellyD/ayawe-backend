7f788caca7f95000542fb5f919674396777224a8
kd <niyuhirekderlin@gmail.com>
2026-05-04 14:29:50 +0200

    target serializer

--- apps/management/serializers.py
+++ apps/management/serializers.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models.account import Account
 from .models.category import Category
+from .models.target import Target
 
 User = get_user_model()
 
@@ -21,10 +22,16 @@ class AccountSerializer(serializers.ModelSerializer):
         validated_data['user'] = user
         return super().create(validated_data)
 
-
 class CategorySerializer(serializers.ModelSerializer):
     type_display = serializers.CharField(source='get_type_display', read_only=True)
 
     class Meta:
         model = Category
         fields = ['id', 'name', 'type', 'type_display', 'icon_name']
+
+class TargetSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Target
+        fields = ['id', 'name', 'target_amount', 'total_amount_saved', 'created_at', 'start_target_date', 'end_target_date']
+        read_only_fields = ['created_at', ]
