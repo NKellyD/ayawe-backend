ac20c4ed299d2f6f37c86ab5a6caed32f9c75e4d
kd <niyuhirekderlin@gmail.com>
2026-05-13 11:38:06 +0200

    endpoint

--- apps/management/urls.py
+++ apps/management/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
-from .views import AccountView,CategoryTypeListView,CategoryView
+from django.urls import path,include
+from rest_framework import routers
+
+from .views import AccountView,CategoryTypeListView,CategoryView,TargetView,ContributionView
+
+router = routers.DefaultRouter()
+router.register('targets', TargetView, basename='target')
+router.register('contributions', ContributionView, basename='contribution')
 
 urlpatterns = [
     path('account/', AccountView.as_view(), name='account'),
     path('categories-type/', CategoryTypeListView.as_view(), name='categories-type'),
     path('categories/', CategoryView.as_view(), name='categories'),
+    path("", include(router.urls)),
 ]
+
