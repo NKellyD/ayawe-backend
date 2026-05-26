ba516c7df18dc4b6932f0326a42590ef96459527
kd <niyuhirekderlin@gmail.com>
2026-05-26 06:54:33 +0200

    mpdel UserEmail

--- apps/authentification/models.py
+++ apps/authentification/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
-from django.db import models
+from django.db import models,transaction
+from django.core.exceptions import ValidationError
+from django.db.models import Q
 
 class UserManager(BaseUserManager):
     def create_user(self, email, username, password=None, **extra_fields):
@@ -39,4 +41,39 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+    @transaction.atomic
+    def add_email(self, email, is_primary=False):
+        if self.emails.filter(email=email).exists():
+            raise ValidationError('Email address already in use')
+        if is_primary:
+            self.emails.update(is_primary=False)
+        return UserEmail.objects.create(user=self,email=email, is_primary=is_primary)
+
+    @transaction.atomic
+    def set_email_primary(self, email_id):
+        self.emails.update(is_primary=False)
+        email = self.emails.get(id=email_id)
+        email.is_primary = True
+        email.save()
+        return email
+
+
+
+
+class UserEmail(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='emails')
+    email = models.EmailField(unique=True)
+    is_primary = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user'],
+                condition=Q(is_primary=True),
+                name='unique_primary_email_per_user',
+            )
+        ]
 
